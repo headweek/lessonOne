bda7dac3fea6a053294873243b159739ac4cbddd
headweek <34040391+headweek@users.noreply.github.com>
2023-09-26 00:00:33 +0400

    Revert "Первая работа"

--- lesson One/main.swift
+++ lesson One/main.swift
@@ -7,13 +7,5 @@
 
 import Foundation
 
-// ZADANIYE ONE
-
-var vklad: Int = 100000
-let percent: Int = 10
-var qodi: Int = 5
-
-var obshiyDoxod = (((vklad * percent) / 100 ) * qodi) + vklad
-
-print(obshiyDoxod)
+print("Hello, World!")
 
